ce95129a91350f58beab066bad19794e82e2022a
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-01-08 13:31:56 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,35 @@
+class Artist
 
+    attr_reader :name
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_song(name, genre)
+        new_song = Song.new(name, self, genre)
+        new_song
+    end
+
+    def songs
+        Song.all.select{|individual_song| individual_song.artist == self}
+    end
+
+    def genres
+        genre_list = []
+        songs.each do |individual_song|
+            genre_list << individual_song.genre
+        end
+        genre_list.uniq
+    end
+    
+
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,33 @@
+class Patient
 
+    attr_reader :name
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(date, doctor)
+        new_appointment = Appointment.new(date, self, doctor)
+        new_appointment
+    end
+
+    def appointments
+        Appointment.all.select{|ind_appmt| ind_appmt.patient == self}
+    end
+
+    def doctors
+        caregivers = []
+        appointments.each do |ind_appmt|
+            caregivers << ind_appmt.doctor
+        end
+        caregivers.uniq
+    end
+
+end
